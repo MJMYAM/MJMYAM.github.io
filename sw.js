f03a760f479390d001cda8f015ea2c26c12f1292
MJMYAM <q8.hazrd@gmail.com>
2026-06-24 17:05:59 +0300

    PWA: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="wc2026-v4";
+const CACHE="wc2026-v5";
 const ASSETS=["./","./index.html","./icon-192.png","./icon-512.png","./manifest.json"];
 self.addEventListener("install", e=>{
   self.skipWaiting();  // فعّل النسخة الجديدة فورًا بدون انتظار
