b4380f9dd60dd542c1ba95e348f1481f4c5e22d5
MJMYAM <q8.hazrd@gmail.com>
2026-06-24 09:43:27 +0300

    PWA: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="wc2026-v1";
+const CACHE="wc2026-v2";
 const ASSETS=["./","./index.html","./icon-192.png","./icon-512.png","./manifest.json"];
 self.addEventListener("install", e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
