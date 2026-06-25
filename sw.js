7af6d36c7238298038a1a10e501b7a78d4ab902f
MJMYAM <q8.hazrd@gmail.com>
2026-06-25 15:30:38 +0300

    PWA: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // إصدار جديد يجبر مسح كل الكاش القديم
-const CACHE="wc2026-v25";
+const CACHE="wc2026-v26";
 self.addEventListener("install", e=>{ self.skipWaiting(); });
 self.addEventListener("activate", e=>{
   e.waitUntil(
