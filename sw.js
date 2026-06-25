dc42a203705093697336c1be90c8620ed3bb9545
MJMYAM <q8.hazrd@gmail.com>
2026-06-26 01:49:50 +0300

    update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // إصدار جديد يجبر مسح كل الكاش القديم
-const CACHE="wc2026-v28";
+const CACHE="wc2026-v29";
 self.addEventListener("install", e=>{ self.skipWaiting(); });
 self.addEventListener("activate", e=>{
   e.waitUntil(
