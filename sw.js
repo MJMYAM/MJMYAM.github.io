35ccbe8eb34e84e437df0fb15d65cfa2049d1c50
MJMYAM <q8.hazrd@gmail.com>
2026-06-27 05:06:13 +0300

    fix: robust goal scorer name extraction (v35)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // إصدار جديد يجبر مسح كل الكاش القديم
-const CACHE="wc2026-v34";
+const CACHE="wc2026-v35";
 self.addEventListener("install", e=>{ self.skipWaiting(); });
 self.addEventListener("activate", e=>{
   e.waitUntil(
